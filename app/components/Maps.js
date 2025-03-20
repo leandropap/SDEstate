b20ec2cf7f43668284b3c1150a36a7ea852300e2
Leandro Pappalardo <leandropap@gmail.com>
2025-03-20 19:12:36 -0300

    Leaflet maps funcionando

--- app/components/Maps.js
+++ app/components/Maps.js
@@ -1,28 +1,20 @@
 'use client'
-
-import { useState } from "react"
-import {
-    APIProvider,
-    Map,
-    AdvancedMarker,
-    InfoWindow
-} from '@vis.gl/react-google-maps'
-
+import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet"
 
 export default function Maps() {
-    const position = { lat: -27.7831, lng: -64.2713 }
-    const [open, setOpen] = useState(false)
+    const centerPos = [-27.7831, -64.2713]
+
 
     return (
-        <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
-            <div style={{ height: '75vh', width: '60%' }} className="border border-black mx-8">
-                <Map center={position} zoom={18} mapId={process.env.NEXT_PUBLIC_GOOGLE_MAPS_MAP_ID}>
-                    <AdvancedMarker position={position} onClick={() => setOpen(true)} />
-                    {open && <InfoWindow position={position} onCloseClick={() => setOpen(false)}>
-                        <p>Tu propiedad</p>
-                    </InfoWindow>}
-                </Map>
-            </div>
-        </APIProvider>
+        <MapContainer center={centerPos} zoom={16} scrollWheelZoom={true} style={{ width: '70%', height: '75vh' }}
+            className="mx-4 border border-black">
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={centerPos}>
+                <Popup>
+                </Popup>
+            </Marker>
+        </MapContainer>
     )
 }
